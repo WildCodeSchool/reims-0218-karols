2b56a166a3ec21a7e7bf07059fa5f375cedf54c2
Arno Wilder Reims <arnocrea@gmail.com>
2018-06-01 17:01:57 +0200

    add new datepicker

--- karols-frontend/src/components/Page.js
+++ karols-frontend/src/components/Page.js
@@ -55,7 +55,7 @@ class Page extends Component {
         </Row>
         <hr />
         <Row>
-          <Col className="text-center">
+          <Col>
             <PreferredDateChoice />
           </Col>
         </Row>
--- karols-frontend/src/components/PreferredDateChoice.js
+++ karols-frontend/src/components/PreferredDateChoice.js
@@ -1,14 +1,65 @@
-import React from "react"
+import React, { Component } from "react"
+import InfiniteCalendar from "react-infinite-calendar"
+import "react-infinite-calendar/styles.css" // Make sure to import the default stylesheet
 
-const PreferredDateChoice = () => {
-  return (
-    <div className="text-center mb-5">
-      <form>
-        <p>Sélectionnez votre date souhaitée :</p>
-        <input type="date" name="preferreddatechoice" />
-      </form>
-    </div>
-  )
+// Render the Calendar
+var today = new Date()
+var lastWeek = new Date(
+  today.getFullYear(),
+  today.getMonth(),
+  today.getDate() - 7
+)
+
+export default class PreferredDateChoice extends Component {
+  state = {
+    date: new Date()
+  }
+
+  onChange = date => this.setState({ date })
+
+  render() {
+    return (
+      <div className="justify-content-center">
+        <InfiniteCalendar
+          style={{ margin: "auto" }}
+          width={400}
+          height={400}
+          selected={today}
+          disabledDays={[0, 7]}
+          minDate={lastWeek}
+          displayOptions={{
+            showHeader: true,
+            overscanMonthCount: 4,
+            showWeekdays: true,
+            shouldHeaderAnimate: true
+          }}
+          locale={{
+            weekStartsOn: 1,
+            locale: require("date-fns/locale/fr"),
+            headerFormat: "dddd, D MMM",
+            weekdays: ["Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam"],
+            blank: "Aucune date sélectionnée",
+            todayLabel: {
+              long: "Aujourd'hui",
+              short: "Auj."
+            }
+          }}
+          theme={{
+            selectionColor: "rgb(146, 118, 255)",
+            textColor: {
+              default: "#333",
+              active: "#FFF"
+            },
+            weekdayColor: "rgb(146, 118, 255)",
+            headerColor: "rgb(127, 95, 251)",
+            floatingNav: {
+              background: "rgba(81, 67, 138, 0.96)",
+              color: "#FFF",
+              chevron: "#FFA726"
+            }
+          }}
+        />
+      </div>
+    )
+  }
 }
-
-export default PreferredDateChoice
