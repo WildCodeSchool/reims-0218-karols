5214e3bfec2ba5f5672a3d8496ec5962ecefebfc
GalloisMax <max.gallois@outlook.fr>
2018-06-08 13:45:03 +0200

    explication reducer gender

--- karols-frontend/src/reducers/genders.js
+++ karols-frontend/src/reducers/genders.js
@@ -12,13 +12,19 @@ const initialState = [
 ]
 
 const genders = (prevState = initialState, action) => {
+  //  déclaration d'une fonction qui prend en parametre prevstate qui est égal a initalstate / action
   if (action.type === CHOOSE_SEX) {
+    // si le type de action est égal a choose_sex (c'est un teste)
     return prevState.map(sexObject => ({
+      // je retourne le prevstate et  parcour element par element qui prend en parametre sexObjet
       ...sexObject,
+      // je décompose l'objet sexObjet
       selected: action.sex === sexObject.sex
+      // dans la proprietée selected je compare le parametre sex de mon action à la proprieté sex de mon objet sexObjet
     }))
   }
   return prevState
+  // sinon je retourne  prevstate
 }
 
 export default genders
