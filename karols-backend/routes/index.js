43a1a8bdf8abd153a46e81aff190cb8e8fa6b5a1
Cynnah <massot.dorian@gmail.com>
2018-06-21 23:13:09 +0200

    creation of path reservation

--- karols-backend/routes/index.js
+++ karols-backend/routes/index.js
@@ -8,6 +8,12 @@ router.get("/", function(req, res, next) {
   res.render("index", { title: "Express" });
 });
 
+// POST home page.
+
+router.post("/", function(req, res) {
+  res.send("POST request to the homepage");
+});
+
 const test = {
   name: "khalid",
   say: "Hello"
@@ -19,6 +25,10 @@ router.get("/test", (req, res) => {
 
 router.get("/prestations", (req, res) => {
   res.json(prestations);
+});
+
+router.post("/reservation", (req, res) => {
+  res.send("coucou");
 });
 
 module.exports = router;
