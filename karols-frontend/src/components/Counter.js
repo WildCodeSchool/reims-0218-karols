9c61864394bf092417e9b1c712d48696308ca653
Anahita <anahita.vahdani@neuf.fr>
2018-06-27 12:00:10 +0200

    create counter component

---
+++ karols-frontend/src/components/Counter.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Button } from "reactstrap"
+
+const Counter = () => {
+  return (
+    <div>
+      <Button size="sm">-</Button>
+      <p>count</p>
+      <Button size="sm">+</Button>
+    </div>
+  )
+}
+
+export default Counter
